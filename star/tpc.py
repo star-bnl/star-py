c9b703f4712c0b7bc79595bdf5c9d09e92539c97
Dmitri Smirnov <d.s@plexoos.com>
2019-04-05 18:33:51 -0400

    Add function to convert angles to TPC sector IDs

--- star/tpc.py
+++ star/tpc.py
@@ -12,3 +12,19 @@ SECTOR_IDS_EAST = 24 - SECTOR_IDS_WEST % 12
 
 SECTORS_EAST = np.append(SECTOR_IDS_EAST, SECTOR_IDS_EAST[0])
 SECTORS_WEST = np.append(SECTOR_IDS_WEST, SECTOR_IDS_WEST[0])
+
+
+def sectors(angles, half='east'):
+    """
+    Returns TPC sector IDs corresponding to the given angles
+    """
+    # Each angle is tested to lie between sector boundaries, i.e. _start <= angle < _stop
+    tpc_sector_indices = np.less_equal(_starts, angles[:, np.newaxis]) & np.greater(_stops, angles[:, np.newaxis])
+    # Get indices of non-zero elements along the second dimension
+    tpc_sector_indices = tpc_sector_indices.nonzero()[1]
+    if half == 'east':
+        return np.fromiter((SECTORS_EAST[i] for i in tpc_sector_indices), 'int64', len(tpc_sector_indices))
+    elif half == 'west':
+        return np.fromiter((SECTORS_WEST[i] for i in tpc_sector_indices), 'int64', len(tpc_sector_indices))
+    else:
+        return None
